f1435ff9884c277541455cb90510e4763100ff22
tommyrac <155997776+tommyrac@users.noreply.github.com>
2024-09-22 19:53:28 -0600

    Update web.sh

--- iptables/web.sh
+++ iptables/web.sh
@@ -22,9 +22,13 @@ sudo iptables -A INPUT -p tcp --dport 443 -m conntrack --ctstate NEW,ESTABLISHED
 sudo iptables -A OUTPUT -p tcp --sport 80 -m conntrack --ctstate ESTABLISHED -j ACCEPT
 sudo iptables -A OUTPUT -p tcp --sport 443 -m conntrack --ctstate ESTABLISHED -j ACCEPT
 
-# Allow outgoing connections on HTTP (port 80) and HTTPS (port 443) for apt
+# Allow outgoing connections on HTTP (port 80) and HTTPS (port 443) for apt and general web access
 sudo iptables -A OUTPUT -p tcp --dport 80 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
 sudo iptables -A OUTPUT -p tcp --dport 443 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+
+# Allow incoming HTTP and HTTPS responses
+sudo iptables -A INPUT -p tcp --sport 80 -m conntrack --ctstate ESTABLISHED -j ACCEPT
+sudo iptables -A INPUT -p tcp --sport 443 -m conntrack --ctstate ESTABLISHED -j ACCEPT
 
 # Allow outgoing DNS queries and responses for domain resolution
 sudo iptables -A OUTPUT -p udp --dport 53 -j ACCEPT
