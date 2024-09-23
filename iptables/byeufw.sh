d0f6946bc52f671783db5494f1fcfb3bf4846bb7
tommyrac <155997776+tommyrac@users.noreply.github.com>
2024-09-22 20:23:55 -0600

    Create byeufw.sh

---
+++ iptables/byeufw.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo iptables -F
+sudo iptables -X
+sudo iptables -t nat -F
+sudo iptables -t nat -X
+sudo iptables -t mangle -F
+sudo iptables -t mangle -X
+sudo iptables -t raw -F
+sudo iptables -t raw -X
