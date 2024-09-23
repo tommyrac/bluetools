ed01339ef7149507e14e4276c7d2f0fdf2a0cfc2
tommyrac <155997776+tommyrac@users.noreply.github.com>
2024-09-22 19:22:48 -0600

    Update web.sh

--- iptables/web.sh
+++ iptables/web.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-## red team if youre here Ill pay you to leave <3 
-
-## to set up web access for us :)
-sudo iptables -A INPUT -p tcp --sport 443 -m conntrack --ctstate ESTABLISHED -j ACCEPT
-sudo iptables -A OUTPUT -p tcp --dport 443 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
-sudo iptables -A OUTPUT -p udp --dport 53 -j ACCEPT
-sudo iptables -A INPUT -p udp --sport 53 -m conntrack --ctstate ESTABLISHED -j ACCEPT
-sudo iptables -A OUTPUT -p tcp --dport 53 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
-sudo iptables -A INPUT -p tcp --sport 53 -m conntrack --ctstate ESTABLISHED -j ACCEPT
-
-## to keep our webserver accessible 
+# Allow incoming and outgoing HTTP (port 80) and HTTPS (port 443)
 sudo iptables -A INPUT -p tcp --dport 80 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
 sudo iptables -A INPUT -p tcp --dport 443 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+sudo iptables -A OUTPUT -p tcp --sport 80 -m conntrack --ctstate ESTABLISHED -j ACCEPT
+sudo iptables -A OUTPUT -p tcp --sport 443 -m conntrack --ctstate ESTABLISHED -j ACCEPT
+
+# Allow outgoing DNS queries and responses for domain resolution
+sudo iptables -A OUTPUT -p udp --dport 53 -j ACCEPT
+sudo iptables -A OUTPUT -p tcp --dport 53 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+sudo iptables -A INPUT -p udp --sport 53 -m conntrack --ctstate ESTABLISHED -j ACCEPT
+sudo iptables -A INPUT -p tcp --sport 53 -m conntrack --ctstate ESTABLISHED -j ACCEPT
+
+# Block all ICMP traffic
+sudo iptables -A INPUT -p icmp -j DROP
+sudo iptables -A OUTPUT -p icmp -j DROP
