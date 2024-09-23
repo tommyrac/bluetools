051fa065edd5d32d51c99e44dc7dd4f14256bb5e
tommyrac <155997776+tommyrac@users.noreply.github.com>
2024-09-22 19:29:56 -0600

    Update web.sh

--- iptables/web.sh
+++ iptables/web.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
 
-# Allow incoming and outgoing HTTP (port 80) and HTTPS (port 443)
+# Flush existing rules
+sudo iptables -F
+
+# Set default policies to DROP
+sudo iptables -P INPUT DROP
+sudo iptables -P OUTPUT DROP
+sudo iptables -P FORWARD DROP
+
+# Allow outgoing ICMP echo requests (ping) and allow established connections
+sudo iptables -A OUTPUT -p icmp --icmp-type echo-request -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+
+# Allow incoming ICMP echo replies for established connections
+sudo iptables -A INPUT -p icmp --icmp-type echo-reply -m conntrack --ctstate ESTABLISHED -j ACCEPT
+
+# Allow incoming and outgoing HTTP and HTTPS traffic for web server access
 sudo iptables -A INPUT -p tcp --dport 80 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
 sudo iptables -A INPUT -p tcp --dport 443 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
 sudo iptables -A OUTPUT -p tcp --sport 80 -m conntrack --ctstate ESTABLISHED -j ACCEPT
 sudo iptables -A OUTPUT -p tcp --sport 443 -m conntrack --ctstate ESTABLISHED -j ACCEPT
 
-# Allow outgoing DNS queries and responses for domain resolution
+# Allow outgoing DNS queries and responses
 sudo iptables -A OUTPUT -p udp --dport 53 -j ACCEPT
 sudo iptables -A OUTPUT -p tcp --dport 53 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
 sudo iptables -A INPUT -p udp --sport 53 -m conntrack --ctstate ESTABLISHED -j ACCEPT
 sudo iptables -A INPUT -p tcp --sport 53 -m conntrack --ctstate ESTABLISHED -j ACCEPT
-
-# Block all ICMP traffic
-sudo iptables -A INPUT -p icmp -j DROP
-sudo iptables -A OUTPUT -p icmp -j ACCEPT
