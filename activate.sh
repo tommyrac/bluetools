119ae75801765d894c593f612b8aa491b6ffc8c3
tommyrac <155997776+tommyrac@users.noreply.github.com>
2024-09-24 01:27:47 -0600

    Update activate.sh

--- activate.sh
+++ activate.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo chmod u+x iptables/openup.sh iptables/safe.sh iptables/web.sh iptables/byeufw.sh
 sudo chmod u+x firewalld/prepford.sh
-sudo chmod u+x websrv/dir_access.sh
+sudo chmod u+x websrv/dir_block.sh websrv/file_block.sh websrv/locksrv.sh
