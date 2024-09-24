355e79d5a374b7e2ccc0e86d9fb0bf5c40002304
tommyrac <155997776+tommyrac@users.noreply.github.com>
2024-09-24 00:42:08 -0600

    Update dir_access.sh

--- websrv/dir_access.sh
+++ websrv/dir_access.sh
@@ -4,4 +4,4 @@ sudo echo "<Directory /var/www/html>
     Options -Indexes +FollowSymLinks
     AllowOverride None
     Require all granted
-</Directory>" >> /etc/apache2/test.conf
+</Directory>" >> /etc/apache2/apache2.conf
