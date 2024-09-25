1aaf0aa65b3f89f5f8a422b8d981cf37fb411267
tommyrac <155997776+tommyrac@users.noreply.github.com>
2024-09-25 00:41:46 -0600

    Update and rename 2shells.sh to shells.sh

--- checks/shells.sh
+++ checks/shells.sh
@@ -3,3 +3,5 @@ echo "############SSH STATUS#############"
 sudo netstat -tunap | grep 22 
 echo "###########POSSIBLE SHELLS#########" 
 sudo netstat -tunap | grep -E "bash|sh|python|perl|nc|php" 
+echo "###########CURRENT USERS#########" 
+who
