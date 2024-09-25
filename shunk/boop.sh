2edcd479658f1b56d2253f637d896d0cfd156f10
tommyrac <155997776+tommyrac@users.noreply.github.com>
2024-09-25 00:59:35 -0600

    Create boop.sh

---
+++ shunk/boop.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo echo "# ================================
+# System-Wide Custom Alias Definitions
+# ================================
+alias useradd='echo "Creating users is restricted on this system."'
+alias passwd='echo "Password changes are not allowed."'" >> /etc/bash.bashrc
