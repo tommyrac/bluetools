63b7990ac7291cf9c1a63a459bec680f0673d77a
tommyrac <155997776+tommyrac@users.noreply.github.com>
2024-09-25 00:18:20 -0600

    Create shells.sh

---
+++ checks/shells.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "############SSH STATUS#############"
+sudo netstat -tunap | grep 22
+echo "###########POSSIBLE SHELLS#########"
+sudo netstat -tunap | grep -E "bash|sh|python|perl|nc|php"
