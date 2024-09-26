4b0b1e15f6f201a9c18325c14f5f0c44697b01a6
tommyrac <155997776+tommyrac@users.noreply.github.com>
2024-09-25 23:57:47 -0600

    Delete activate.sh

--- activate.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo chmod u+x iptables/openup.sh iptables/safe.sh iptables/web.sh iptables/byeufw.sh
-sudo chmod u+x firewalld/prepford.sh
-sudo chmod u+x websrv/dir_block.sh websrv/file_block.sh websrv/locksrv.sh
-sudo chmod u+x checks/shells.sh
-sudo chmod u+x shunk/boop.sh
