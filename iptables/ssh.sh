946b8f520c3850fc62d7663baf52a460cc16538e
tommyrac <155997776+tommyrac@users.noreply.github.com>
2024-09-28 01:43:23 -0600

    Create ssh.sh

---
+++ iptables/ssh.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo iptables -A INPUT -p tcp --dport 22 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+sudo iptables -A OUTPUT -p tcp --sport 22 -m conntrack --ctstate ESTABLISHED -j ACCEPT
+
