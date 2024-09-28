5780e9616f27f98f0920030bdf714be8217865c3
tommyrac <155997776+tommyrac@users.noreply.github.com>
2024-09-28 13:22:02 -0600

    Create splunkwget.sh

---
+++ splunkwget.sh
@@ -0,0 +1,3 @@
+#!/bin/bash 
+
+sudo wget -O splunkforwarder-9.3.1-ee2724eb9973.s390x.rpm "https://download.splunk.com/products/universalforwarder/releases/9.3.1/linux/splunkforwarder-9.3.1-ee2724eb9973.s390x.rpm"
