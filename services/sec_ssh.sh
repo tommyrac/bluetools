6c53cfbeda83664522f92a6f38e0ed2dddb90525
tommyrac <155997776+tommyrac@users.noreply.github.com>
2024-09-28 02:04:54 -0600

    Update sec_ssh.sh

--- services/sec_ssh.sh
+++ services/sec_ssh.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-sudo echo "AllowUsers vagrant boba_fett" >> /etc/ssh/sshd_config
+sudo echo "AllowUsers vagrant boba_fett
+ClientAliveInterval 20
+ClientAliveCountMax 0
+MaxAuthTries 2" >> /etc/ssh/sshd_config
