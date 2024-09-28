687063c93d831f29fbdc5ac2752a2102cb6df8ce
tommyrac <155997776+tommyrac@users.noreply.github.com>
2024-09-28 02:48:05 -0600

    Update activate.sh

--- activate.sh
+++ activate.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-sudo chmod u+x iptables/openup.sh iptables/safe.sh iptables/web.sh iptables/byeufw.sh
+sudo chmod u+x iptables/openup.sh iptables/safe.sh iptables/web.sh iptables/byeufw.sh iptables/ssh.sh iptables/first.sh
 sudo chmod u+x firewalld/prepford.sh
-sudo chmod u+x websrv/dir_block.sh websrv/file_block.sh websrv/locksrv.sh
-sudo chmod u+x checks/shells.sh
+sudo chmod u+x websrv/dir_block.sh websrv/file_block.sh websrv/locksrv.sh websrv/fix_pay.sh
+sudo chmod u+x checks/shells.sh checks/full.sh
 sudo chmod u+x shunk/boop.sh
+sudo chmod u+x files/lockup.sh files/sicherung.sh files/ssh_permish.sh 
+sudo chmod u+x services/ftpfix.sh services/proftpd.conf services/sec_ssh.sh
