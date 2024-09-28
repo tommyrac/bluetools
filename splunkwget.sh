312b36f1c3925222578049c4b0d2afeaedd37756
tommyrac <155997776+tommyrac@users.noreply.github.com>
2024-09-28 13:34:57 -0600

    Update splunkwget.sh

--- splunkwget.sh
+++ splunkwget.sh
@@ -1,3 +1,2 @@
-#!/bin/bash 
-
-sudo wget -O splunkforwarder-9.3.1-ee2724eb9973.s390x.rpm "https://download.splunk.com/products/universalforwarder/releases/9.3.1/linux/splunkforwarder-9.3.1-ee2724eb9973.s390x.rpm"
+#!/bin/bash
+wget -O splunkforwarder-9.3.1-0b8d769cb912.x86_64.rpm "https://download.splunk.com/products/universalforwarder/releases/9.3.1/linux/splunkforwarder-9.3.1-0b8d769cb912.x86_64.rpm"
