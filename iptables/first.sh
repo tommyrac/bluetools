2da8bd76cb1099b1ee431e698f5e9868fca6a434
tommyrac <155997776+tommyrac@users.noreply.github.com>
2024-09-28 00:38:59 -0600

    Update first.sh

--- iptables/first.sh
+++ iptables/first.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo iptables-save > restore.txt
-sudo iptbales-save > /usr/share/gutentag/sicherung/restoretables.txt
+sudo iptables-save > /usr/share/gutentag/sicherung/restoretables.txt
